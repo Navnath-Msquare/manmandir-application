1c06b03e0c794fc63992960718ad77d7732f8b23
Navnath-Msquare <navnath.b@msquaresoftware.com>
2026-07-01 10:44:18 +0530

    new latest code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  baseURL:'https://api.zerofollowup.com/',
+  //base url
+  baseURL: 'https://api.zerofollowup.com/',
   // baseURL: 'https://api.karobooking.com/',
-  razorpayKey : 'rzp_live_kgwQM75Yree4AU',
+  razorpayKey: 'rzp_live_kgwQM75Yree4AU',
   razorpayKeySecret: 'CNYXoyj3o3E0IYWDHgP0FSDy',
   busApi: 'https://partnerapi.iamgds.com/ota/',
   busTranApi: 'https://partnertranapi.iamgds.com/ota/',
