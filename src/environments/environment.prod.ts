bc65ba8f1f3edb9f74b8c2531f019bca614f347a
Navnath-Msquare <navnath.b@msquaresoftware.com>
2026-07-16 16:47:53 +0530

    razor[ay test key add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,13 @@ export const environment = {
   //base url
   baseURL: 'https://api.zerofollowup.com/',
   // baseURL: 'https://api.karobooking.com/',
-  razorpayKey: 'rzp_live_kgwQM75Yree4AU',
-  razorpayKeySecret: 'CNYXoyj3o3E0IYWDHgP0FSDy',
+ // razorpayKey: 'rzp_live_kgwQM75Yree4AU',
+  //razorpayKeySecret: 'CNYXoyj3o3E0IYWDHgP0FSDy',
 
-  //  razorpayKey: 'rzp_test_T7oLtzYNTFi1kd',
-  // razorpayKeySecret: '1rELcPUDZCn9YrHJrd6PFVNV',
+ // Razorpay test key and secret key
+  razorpayKey: 'rzp_test_TDG1MR7TQnALrl',
+  razorpayKeySecret: 'MF3Ui389lxw5K63Zkx0SQakX',
+  
   busApi: 'https://partnerapi.iamgds.com/ota/',
   busTranApi: 'https://partnertranapi.iamgds.com/ota/',
   newApikey: 'TSHSPFAPI58368531'
